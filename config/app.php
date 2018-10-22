fd120e8bfd6497ad9f7cabafa95f6387826d8e57
Ahmad Choirun Najib <najibsangar@gmail.com>
2018-10-22 20:38:18 +0700

    add libraries htmlcollective and activestate

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Pyaesone17\ActiveState\ActiveStateServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -221,6 +223,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Active' => Pyaesone17\ActiveState\ActiveFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
